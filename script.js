469fde0b0e41bf89d56f1ca728515755b3eb1e29
RMarin0810 <reynaldomarin6@gmail.com>
2024-08-31 23:06:12 -0500

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Configuración de Supabase
-const supabaseUrl = 'https://tsiiqfmwyjyufpnxsnps.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzaWlxZm13eWp5dWZwbnhzbnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4OTcyNzMsImV4cCI6MjA0MDQ3MzI3M30.rwXtg9LCO1g_Us2BdrXwaK4Y0UfD-Fndax7RPYQgtkI';
-const supabase = Supabase.createClient(supabaseUrl, supabaseKey);
+// Configuración de Supabase
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://tsiiqfmwyjyufpnxsnps.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 let selectedTaskId = null;
 
